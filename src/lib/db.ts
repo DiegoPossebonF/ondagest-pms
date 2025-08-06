ab5e4ea723601bf7feaddac17568020bd2602e3f
Diego Possebon Fernandes <diegopossebonf@gmail.com>
2025-08-06 16:28:57 -0300

    refactor: simplifica a inicialização do Prisma e remove código redundante

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,7 @@
 import { PrismaClient } from '@/app/generated/prisma'
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-const prisma = new PrismaClient()
+export const prisma = globalForPrisma.prisma || new PrismaClient()
 
-const db = globalForPrisma.prisma || prisma
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
-
-export default db
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
