87e178d62152c4f31abd6629b0c2f3a25de089ac
Diego Possebon Fernandes <diegopossebonf@gmail.com>
2025-08-13 14:56:54 -0300

    refactor: remove componente filho desnecessario de SignupCard na SignupPage

--- src/app/(public)/(auth)/signup/page.tsx
+++ src/app/(public)/(auth)/signup/page.tsx
@@ -1,12 +1,9 @@
 import { SignupCard } from '@/components/auth/SignupCard'
-import { SignupForm } from '@/components/auth/SignupForm'
 
 export default function SignupPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-blue-200">
-      <SignupCard>
-        <SignupForm />
-      </SignupCard>
+      <SignupCard />
     </div>
   )
 }
